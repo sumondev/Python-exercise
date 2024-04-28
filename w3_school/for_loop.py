fb0ad52756b7c7a472273773cc394fc0ffe0b60d
sumondev <sumon.diu.cse@gmail.com>
2024-04-28 16:33:30 +0200

    Basic for loop example

---
+++ w3_school/for_loop.py
@@ -0,0 +1,34 @@
+# n = int(input("Input a number: "))
+
+# for a in range(n):
+#         print(a)
+
+# for a in range(1, n + 1):
+#         print(a, end=" ")
+
+
+# sum =0
+# for a in range(0, n+1):
+#         sum = sum +a
+#         print(sum)
+
+
+# Assending  Order
+
+# The syntax of the range function is as follows:
+# for i in range(start, stop, step):
+# For example, if the value of start = 0 and stop = length of the required list and step= 2, then the for loop will iterate over every second element in the entire list
+
+# n = int (input("Input a number"))
+
+# for a in range (0,n+1,2):
+     
+#         print( a) 
+
+#Decending Order
+
+n = int(input("Input a number"))
+for a in range(n,-1,-1): 
+
+    print (a)
+    # here 1st n=input number, then it decrese to 0 that;s why write -1, and decase every step -1.
