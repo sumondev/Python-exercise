68a4877af97ead4a680b156221bd1d9f38b18c14
sumondev <sumon.diu.cse@gmail.com>
2024-05-04 13:04:11 +0200

    python string slicing modified

--- w3_school/String_operation/string_opration.py
+++ w3_school/String_operation/string_opration.py
@@ -6,6 +6,7 @@ print(v1[0:]) #it  printed from 0 to last of the element, Slice to the end
 print(v1[-1:0]) # it slicing last to frist but in acending order.
 print(v1[0:-1]) # it slicng 0 to before last element
 print(v1[-1:-5]) # it slicing is also not be executed
-print(v1[-5:-1]) # this slicing also empty because of range is not in order
+print(v1[-5:-1]) # this slicing also empty because of range is not in order 
 print(v1[-5:1]) # this kind of slicing have empty because of range is over boundary
 
+#here all are exapmle is string slicing.
