dd44195e4e41096380b24e61a136d2220d197a66
Md Motaleb Hossain <motaleb.hossain.sumon@gmail.com>
2024-05-08 01:56:08 +0200

    add list examples\

---
+++ w3_school/Python_list/add_list.py
@@ -0,0 +1,10 @@
+#To add an item to the end of the list, use the append() method:
+
+f_list=["Banana","Apple","Coconut"]
+f_list.append("Orange")
+print(f_list)
+
+#To insert a list item at a specified index, use the insert() method.
+f_list=["Apple","coconut"]
+f_list.insert(1,"Mangoes")
+
