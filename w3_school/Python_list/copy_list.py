f6fd65f38d56865dc3d3874afcce998ca3627dc3
sumondev <motaleb.hossain.sumon@gmail.com>
2024-05-11 23:58:35 +0200

    copy and list example added

--- w3_school/Python_list/copy_list.py
+++ w3_school/Python_list/copy_list.py
@@ -2,3 +2,8 @@
 thisList = ["apple", "banana", "cherry", "mango", "kiwi"]
 myList= thisList.copy()
 print(thisList)
+
+
+thisList = ["apple", "banana", "cherry", "mango", "kiwi"]
+myList = list (thisList)
+print(myList)
