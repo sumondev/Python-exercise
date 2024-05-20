38af014ca6458f8c12b03b77190fc63d26a6963b
Md Motaleb Hossain <motaleb.hossain.sumon@gmail.com>
2024-05-21 00:00:06 +0200

    updated

--- w3_school/Python_tuples/access_tuple.py
+++ w3_school/Python_tuples/access_tuple.py
@@ -17,5 +17,5 @@ thislist = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
 if "apple" in thislist:
   print("Yes, 'apple' is in the fruits list")
 
-thislist= (1,2,3,4,5,6,7,8,9)
+thislist= (1,2,3,4,5,6,7,8,9,10)
 print(thislist[1:6:2]) # last perameter 2 is the step 
