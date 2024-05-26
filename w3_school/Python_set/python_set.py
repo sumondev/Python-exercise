2f777cd7d92d38651f90e618baa6947181402e44
MD Motaleb Hossain <motaleb.hossain.sumon@gmail.com>
2024-05-27 01:54:14 +0200

    updated

--- w3_school/Python_set/python_set.py
+++ w3_school/Python_set/python_set.py
@@ -26,9 +26,13 @@ print(len(thisset))
 
 #String, int and boolean data types:
 set1 = {"apple", "banana", "cherry"}
-set2 = {1, 5, 7, 9, 3}
+set2 = {1, 5, 7, 9, 3,7}
 set3 = {True, False, False}
 
 print(set1)
 print(set2)
 print(set3)
+
+#A set with strings, integers and boolean values:
+set1 = {"abc", 34, True, 40, "male"}
+print(set1)
