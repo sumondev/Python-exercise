055a30e2591cbf37c817f9d1a5a25dbac766b615
Md Motaleb Hossain <motaleb.hossain.sumon@gmail.com>
2024-05-26 23:59:27 +0200

    modified

--- w3_school/Python_list/Python_set/python_set.py
+++ w3_school/Python_list/Python_set/python_set.py
@@ -23,3 +23,12 @@ print(thisset)
 thisset = {"apple", "banana", "cherry"}
 
 print(len(thisset))
+
+#String, int and boolean data types:
+set1 = {"apple", "banana", "cherry"}
+set2 = {1, 5, 7, 9, 3}
+set3 = {True, False, False}
+
+print(set1)
+print(set2)
+print(set3)
