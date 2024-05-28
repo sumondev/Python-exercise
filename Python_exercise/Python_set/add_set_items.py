cb2734aa6f247539d3fdc8e2c4c83ac7ef8e754c
MD Motaleb Hossain <motaleb.hossain.sumon@gmail.com>
2024-05-28 23:52:31 +0200

    update and list is added to set

--- Python_exercise/Python_set/add_set_items.py
+++ Python_exercise/Python_set/add_set_items.py
@@ -3,3 +3,18 @@
 thisset = {"apple", "banana", "cherry"}
 thisset.add("orange")
 print(thisset)
+
+#to add items from another set into the current set, use the update() method.
+
+thisset = {"apple", "banana", "cherry"}
+tropical = {"pineapple", "mango", "papaya"}
+thisset.update(tropical)
+print(thisset)
+
+#Add elements of a list to at set:
+thisset = {"apple", "banana", "cherry"}
+mylist = ["kiwi", "orange"]
+
+thisset.update(mylist)
+
+print(thisset)
