06d38caf0764ac90a6a6266ca6b533926b482a75
MD Motaleb Hossain <motaleb.hossain.sumon@gmail.com>
2024-05-29 01:38:25 +0200

    differnce method of set

--- Python_exercise/Python_set/join_set.py
+++ Python_exercise/Python_set/join_set.py
@@ -60,3 +60,33 @@ set2 = {False, "google", "apple", 2, True}
 set3 = set1.intersection(set2)
 
 print(set3)
+
+
+#The difference() method will return a new set that will contain only the items from the first set that are not present in the other se
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+set3=set1.difference(set2)
+print(set3)
+#we can use - oprator for geting the same result
+
+set4 = set1-set2
+print(set4)
+
+#diiference_update is also same but it store the value of begening set which is started first.
+set1.difference_update(set2)
+print(set1)
+
+#The symmetric_difference() method will keep only the elements that are NOT present in both sets.
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+set3=set1.symmetric_difference(set2)
+print(set3)
+
+#we can use the ^ operator instead of the symmetric_difference() method, and you will get the same result.
+set4 = set1 ^ set2
+print(set4)
+
+#The symmetric_difference_update() method will also keep all but the duplicates, but it will change the original set instead of returning a new set.
+set1.symmetric_difference_update(set2)
+
+print(set1)
