68c9cd2ef428c79f4fa9955670647c630a1ce740
MD Motaleb Hossain <motaleb.hossain.sumon@gmail.com>
2024-05-28 23:59:15 +0200

    discard method

--- Python_exercise/Python_set/remove_set_item.py
+++ Python_exercise/Python_set/remove_set_item.py
@@ -1,6 +1,8 @@
 #To remove an item in a set, use the remove(), or the discard() method
 thisset = {"apple", "banana", "cherry"}
-
 thisset.remove("banana")
+print(thisset)
 
+thisset = {"apple", "banana", "cherry"}
+thisset.discard("banana")
 print(thisset)
