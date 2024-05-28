dc983cea27be92a32fa1f9cef607012b3077ce49
MD Motaleb Hossain <motaleb.hossain.sumon@gmail.com>
2024-05-29 01:03:49 +0200

    join method updated \

--- Python_exercise/Python_set/add_set_items.py
+++ Python_exercise/Python_set/add_set_items.py
@@ -14,7 +14,5 @@ print(thisset)
 #Add elements of a list to at set:
 thisset = {"apple", "banana", "cherry"}
 mylist = ["kiwi", "orange"]
-
 thisset.update(mylist)
-
 print(thisset)
---
+++ Python_exercise/Python_set/join_set.py
@@ -0,0 +1,13 @@
+#The union() method allows you to join a set with other data types, like lists or tuples.
+x = {"a","b","c"}
+y = {1,2,3}
+z=x.union(y)
+print(z)
+
+x = {"a","b","c"}
+y = {1,2,3}
+z=x|y
+print(z)
+
+#the | operator is also same as join operator
+
