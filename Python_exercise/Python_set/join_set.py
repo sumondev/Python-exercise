4c6677f26b178d8aa358745624f46d6e2ef2dc8f
MD Motaleb Hossain <motaleb.hossain.sumon@gmail.com>
2024-05-29 01:19:31 +0200

    multiple union set

--- Python_exercise/Python_set/join_set.py
+++ Python_exercise/Python_set/join_set.py
@@ -8,6 +8,15 @@ x = {"a","b","c"}
 y = {1,2,3}
 z=x|y
 print(z)
-
 #the | operator is also same as join operator
 
+set1 = {"a", "b", "c"}
+set2 = {1, 2, 3}
+set3 = {"John", "Elena"}
+set4 = {"apple", "bananas", "cherry"}
+
+myset = set1.union(set2, set3, set4)
+print(myset)
+
+myset= set1 | set2 |set3 |set4
+print(myset)
