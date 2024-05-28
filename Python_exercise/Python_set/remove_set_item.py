9fd9632266be6e32335cc21b27ecc832241b9b28
MD Motaleb Hossain <motaleb.hossain.sumon@gmail.com>
2024-05-29 00:01:39 +0200

    pop method is set

--- Python_exercise/Python_set/remove_set_item.py
+++ Python_exercise/Python_set/remove_set_item.py
@@ -6,3 +6,10 @@ print(thisset)
 thisset = {"apple", "banana", "cherry"}
 thisset.discard("banana")
 print(thisset)
+#If the item to remove does not exist, discard() will NOT raise an error.
+
+#You can also use the pop() method to remove an item, but this method will remove a random item, so you cannot be sure what item that gets removed.
+thisset = {"apple", "banana", "cherry"}
+x = thisset.pop()
+print(x)
+print(thisset)
