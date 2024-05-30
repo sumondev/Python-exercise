443bd86e8016113d2f421f17337f298c3a4da56f
MD Motaleb Hossain <motaleb.hossain.sumon@gmail.com>
2024-05-30 23:59:08 +0200

    Duplicated value over write

--- Python_exercise/Python _dictionaries/python _dctionaries.py
+++ Python_exercise/Python _dictionaries/python _dctionaries.py
@@ -11,3 +11,12 @@ thisdict = {
   "year": 1964
 }
 print(thisdict["brand"])
+
+#Duplicate values will overwrite existing values:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "year": 2020
+}
+print(thisdict)
