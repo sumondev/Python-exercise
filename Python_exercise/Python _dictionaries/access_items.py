bbbf23ba2c39390015eeb40b1fd5f3bb4e5fd3a9
MD Motaleb Hossain <motaleb.hossain.sumon@gmail.com>
2024-05-31 00:35:55 +0200

    modified

--- Python_exercise/Python _dictionaries/access_items.py
+++ Python_exercise/Python _dictionaries/access_items.py
@@ -15,3 +15,19 @@ x = thisdict.keys()
 print(x)
 #it shows all key of the dictionary
 
+
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+
+x = car.keys()
+
+print(x) #before the change
+
+car["color"] = "white"
+car["year"] = 1996
+
+print(x) #after the change
+print(car.values())
