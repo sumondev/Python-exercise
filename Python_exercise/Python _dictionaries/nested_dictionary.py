1a267da347ea35ceb40a5e968267f34eee7715a4
MD Motaleb Hossain <motaleb.hossain.sumon@gmail.com>
2024-06-02 00:11:14 +0200

    all done

---
+++ Python_exercise/Python _dictionaries/nested_dictionary.py
@@ -0,0 +1,50 @@
+#Create a dictionary that contain three dictionaries:
+myfamily = {
+  "child1" : {
+    "name" : "Emil",
+    "year" : 2004
+  },
+  "child2" : {
+    "name" : "Tobias",
+    "year" : 2007
+  },
+  "child3" : {
+    "name" : "Linus",
+    "year" : 2011
+  }
+}
+print(myfamily)
+print(myfamily["child1"])
+print(myfamily["child2"]["name"])
+
+#loop though all items of dictionary
+
+for x,obj in myfamily.items():
+    print(x)
+
+    for y in obj:
+        print(y + ':', obj[y])
+    # for x,y in obj.items():
+    #     print(x,y) # we can also use this way .
+
+#Create three dictionaries, then create one dictionary that will contain the other three dictionaries:
+child1 = {
+  "name" : "Emil",
+  "year" : 2004
+}
+child2 = {
+  "name" : "Tobias",
+  "year" : 2007
+}
+child3 = {
+  "name" : "Linus",
+  "year" : 2011
+}
+
+myfamily = {
+  "child1" : child1,
+  "child2" : child2,
+  "child3" : child3
+}
+
+print(myfamily)
