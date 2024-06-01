ecbc4d44dc28ece35c89cbae880466bc1b6281b4
MD Motaleb Hossain <motaleb.hossain.sumon@gmail.com>
2024-06-01 23:56:10 +0200

    retive all key and value

--- Python_exercise/Python _dictionaries/loop_dictionary.py
+++ Python_exercise/Python _dictionaries/loop_dictionary.py
@@ -7,6 +7,16 @@ thisdict =	{
 for x in thisdict:
   print(x)
 
+#   for x in thisdict.keys():
+#       print(x) # we can use this method for use to print key values
+
 #Print all values in the dictionary, one by one:
 for x in thisdict:
   print(thisdict[x])
+# for x in thisdict.values():
+#   print(x) # we can also use this method
+
+
+#Loop through both keys and values, by using the items() method:
+for x, y in thisdict.items():
+  print(x, y)
