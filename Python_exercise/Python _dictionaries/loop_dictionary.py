f8f1d01edc609e43d6155b974fdae5b615753328
MD Motaleb Hossain <motaleb.hossain.sumon@gmail.com>
2024-06-01 23:53:02 +0200

    loop dictionary key and values single element

---
+++ Python_exercise/Python _dictionaries/loop_dictionary.py
@@ -0,0 +1,12 @@
+#Print all key names in the dictionary, one by one:
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+for x in thisdict:
+  print(x)
+
+#Print all values in the dictionary, one by one:
+for x in thisdict:
+  print(thisdict[x])
