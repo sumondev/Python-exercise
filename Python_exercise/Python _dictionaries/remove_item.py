86aa22e94af90e4836708db374528aeaf68e9220
MD Motaleb Hossain <motaleb.hossain.sumon@gmail.com>
2024-06-01 23:49:14 +0200

    clear method implemented

--- Python_exercise/Python _dictionaries/remove_item.py
+++ Python_exercise/Python _dictionaries/remove_item.py
@@ -26,3 +26,21 @@ thisdict = {
 }
 del thisdict["model"]
 print(thisdict)
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+#del thisdict
+print(thisdict) #this will cause an error because "thisdict" no longer exists.
+
+
+#The clear() method empties the dictionary:
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.clear()
+print(thisdict)
