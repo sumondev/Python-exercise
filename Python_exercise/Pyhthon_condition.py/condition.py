21909826d27402ee577af07c2443c97142911997
Md Motaleb Hossain <motaleb.hossain.sumon@gmail.com>
2024-06-04 00:47:16 +0200

    codion updated

--- Python_exercise/Pyhthon_condition.py/condition.py
+++ Python_exercise/Pyhthon_condition.py/condition.py
@@ -11,3 +11,13 @@ if b > a:
   print("b is greater than a")
 elif a == b:
   print("a and b are equal")
+
+
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
