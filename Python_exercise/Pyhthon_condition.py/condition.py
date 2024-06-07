6762c2c6f7279e177724a2fda9ca2e74d225eebc
Md Motaleb Hossain <motaleb.hossain.sumon@gmail.com>
2024-06-07 15:23:42 +0200

    orr operator

--- Python_exercise/Pyhthon_condition.py/condition.py
+++ Python_exercise/Pyhthon_condition.py/condition.py
@@ -53,3 +53,11 @@ c = 500
 if a > b and c > a:
   print("Both conditions are True")
 
+
+#or operator
+a = 200
+b = 33
+c = 500
+if a > b or a > c:
+  print("At least one of the conditions is True")
+  
