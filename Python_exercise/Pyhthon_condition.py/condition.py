a7f3bdad965c67c6f8fe268698e5d4968bd7a8ed
Md Motaleb Hossain <motaleb.hossain.sumon@gmail.com>
2024-06-07 15:22:54 +0200

    and operator

--- Python_exercise/Pyhthon_condition.py/condition.py
+++ Python_exercise/Pyhthon_condition.py/condition.py
@@ -40,3 +40,16 @@ else:
   a = 2
 b = 330
 print("A") if a > b else print("B")
+
+#One line if else statement, with 3 conditions:
+a = 330
+b = 330
+print("A") if a > b else print("=") if a == b else print("B")
+
+#And operator 
+a = 200
+b = 33
+c = 500
+if a > b and c > a:
+  print("Both conditions are True")
+
