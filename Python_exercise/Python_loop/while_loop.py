09e67bb242be1e682eca458fc168ae185dc7db83
Md Motaleb Hossain <motaleb.hossain.sumon@gmail.com>
2024-06-08 23:44:50 +0200

    while break

--- Python_exercise/Python_loop/while_loop.py
+++ Python_exercise/Python_loop/while_loop.py
@@ -1,11 +1,22 @@
 #incresing..
+print("incresing")
 i = 1
 while i < 6:
   print(i)
   i += 1
 
 #Decreasing....
+print("Decresing...")
 i=6
 while i>=1:
  print(i)
  i -=1
+
+#Break
+print("break")
+i = 1
+while i < 6:
+  print(i)
+  if i==4:
+        break
+  i += 1
