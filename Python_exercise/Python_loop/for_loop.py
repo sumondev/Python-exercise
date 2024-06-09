50e2c8ba034d2b6de4ad9da4ac9f88bf896ec631
Md Motaleb Hossain <motaleb.hossain.sumon@gmail.com>
2024-06-09 23:57:58 +0200

    exit loop in letter break

--- Python_exercise/Python_loop/for_loop.py
+++ Python_exercise/Python_loop/for_loop.py
@@ -15,3 +15,11 @@ for x in fruits:
   print(x)
   if x == "banana":
     break 
+
+
+print("Exit the loop when x is banana, but this time the break comes before the print:")
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x) 
