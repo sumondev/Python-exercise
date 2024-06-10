8898cb67ac794d5c0392b6ef3e4b062c53928dfe
Md Motaleb Hossain <motaleb.hossain.sumon@gmail.com>
2024-06-10 23:54:00 +0200

    Increment the sequence with 3 (default is 1):

--- Python_exercise/Python_loop/for_loop.py
+++ Python_exercise/Python_loop/for_loop.py
@@ -38,3 +38,7 @@ for x in range(6):
 print("using start perameter")
 for x in range(2, 6):
   print(x)  
+
+print("Increment the sequence with 3 (default is 1):")
+for x in range(2, 30, 3):
+  print(x)  
