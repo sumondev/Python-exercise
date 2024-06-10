2ee82e230df731c470f1246b697ad24ca7f79de8
Md Motaleb Hossain <motaleb.hossain.sumon@gmail.com>
2024-06-10 23:58:48 +0200

    If the loop breaks, the else block is not executed.

--- Python_exercise/Python_loop/for_loop.py
+++ Python_exercise/Python_loop/for_loop.py
@@ -48,3 +48,10 @@ for x in range(6):
       print(x)
 else:
   print("Finally finished!")
+
+print("If the loop breaks, the else block is not executed.")
+for x in range(6):
+    if x == 3: break
+    print(x)
+else:
+  print("Finally finished!")
