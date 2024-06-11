52c96a89ac9716e509c42f594781bbc0d1444216
Md Motaleb Hossain <motaleb.hossain.sumon@gmail.com>
2024-06-11 23:27:27 +0200

    The pass statement

--- Python_exercise/Python_loop/for_loop.py
+++ Python_exercise/Python_loop/for_loop.py
@@ -55,3 +55,15 @@ for x in range(6):
     print(x)
 else:
   print("Finally finished!")
+
+print("Nested loop")
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)  
+
+print("The pass Statement")
+for x in [0, 1, 2]:
+      pass    
