32f6fbc2fa4dd00ca13d58c4e8c362399570d3ab
Md Motaleb Hossain <motaleb.hossain.sumon@gmail.com>
2024-06-12 23:50:42 +0200

    2 values 1 arguments get error

--- Python_exercise/Python_function/function.py
+++ Python_exercise/Python_function/function.py
@@ -19,3 +19,9 @@ def my_function(fname, lname):
       print(fname + " " + lname)
 
 my_function("Emil", "Refsnes")
+
+#This function expects 2 arguments, but gets only 1:
+def my_function(fname, lname):
+      print(fname + " " + lname)
+my_function("Emil")
+#it will be get an error
